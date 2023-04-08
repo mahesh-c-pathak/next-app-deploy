3e2e5fbacecd8c1c0379d9128e5ffa4235b71e9c
Mahesh <mahesh.c.pathak@gmail.com>
2023-04-08 12:52:38 +0530

    third attempt

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: True,
+  reactStrictMode: true,
   basePath: '/next-app-deploy',
   assetPrefix: '/next-app-deploy',
 }
